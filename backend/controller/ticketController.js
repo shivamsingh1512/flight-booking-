eb7c544a1439d4da6727e70a20ffb82a1340f703
shivamsingh1512 <bwubta21169@brainwareuniversity.ac.in>
2025-03-16 12:59:05 +0530

    patch ticket cancel completed

--- backend/controller/ticketController.js
+++ backend/controller/ticketController.js
@@ -23,9 +23,37 @@ module.exports.createTicket=async(req,res)=>{
 
 module.exports.getTickets=async(req,res)=>{
     try{
+      const ticket=await Ticket.find({status:"confirm"});
+      res.status(201).send(ticket);
+    }catch(err){
+      console.log(err);
+    }
+  }
+
+  module.exports.ticketsHistory=async(req,res)=>{
+    try{
       const ticket=await Ticket.find({owner:req.user._id});
       res.status(201).send(ticket);
     }catch(err){
       console.log(err);
+    }
+  }
+
+  module.exports.cancelTicket=async(req,res)=>{
+    try{
+      const {ticketID}=req.params;
+      if (!ticketID) {
+        return res.status(400).send("Ticket ID is required");
+      }
+      const ticket=await Ticket.findOne({_id:ticketID,owner:req.user._id});
+      if(!ticket){
+        res.status(401).send("no ticket found");
+      }
+      
+      ticket.status="canceled";
+      await ticket.save();
+      res.status(201).send(ticket);
+    }catch(err){
+        console.log(err);
     }
   }
--- backend/routes/ticketRouter.js
+++ backend/routes/ticketRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const userAuth = require("../middleware/userAuth");
-const { createTicket , getTickets } = require("../controller/ticketController");
+const { createTicket , getTickets ,ticketsHistory , cancelTicket } = require("../controller/ticketController");
 const router=express.Router();
 
 router.post("/create",userAuth,createTicket);
 router.get("/getticket",userAuth,getTickets);
-
+router.get("/history",userAuth,ticketsHistory);
+router.patch("/cancel/:ticketID",userAuth,cancelTicket);
 module.exports=router;
